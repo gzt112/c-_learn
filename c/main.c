35a038852fc32bef38cb125206517dd520dd2f5d
guozhaotian <gzt112@163.com>
2022-08-11 15:19:28 +0800

    Initial Commit

---
+++ c/main.c
@@ -0,0 +1,51 @@
+//
+//  main.c
+//  c
+//
+//  Created by bill on 2022/8/9.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+/*enum sex
+{
+    male,
+    female,
+    secret
+};
+enum color
+{
+    yellow,
+    red,
+    blue
+    
+};*/
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("(are you ok??)\n");
+    printf("%d\n",(int)strlen("c:\test\3277\test.c"));
+//    printf("%c\n",'');
+//    char arr[] = "abc";//数组
+//    char arr1[] = {'a','b','c',};
+//    printf("%s\n",arr);
+//    printf("%s\n",arr1);
+//    printf("%d\n",(int)strlen(arr));
+//    printf("%d\n",(int)strlen(arr1));
+//    printf("Hello, World!\n");
+//    char a[] = {"a","b",};
+//     const int a = 20;
+//    a = 30;
+//    int arr[max] = {0};
+//    printf("%d\n",arr[5]);
+//
+//    enum color coulor = blue;
+//    coulor = yellow;
+//    printf("%d\n",coulor);
+//    printf("%d\n",male);
+//    printf("%d\n",female);
+//    printf("%d\n",secret);
+    
+    return 0;
+}
